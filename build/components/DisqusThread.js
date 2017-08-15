02823bc5b386a12d6915dcf3a4cc547bb17a54be
Daniel Wilhelmsen <dpwilhelmsen@yahoo.com>
2017-08-14 17:32:24 -0700

    Allow the disqus component to accept a sso config object

--- build/components/DisqusThread.js
+++ build/components/DisqusThread.js
@@ -10,7 +10,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var DISQUS_CONFIG = ['shortname', 'identifier', 'title', 'url', 'category_id', 'onNewComment', 'api_key', 'remote_auth_s3', 'targetWindow'];
+var DISQUS_CONFIG = ['shortname', 'identifier', 'title', 'url', 'category_id', 'onNewComment', 'api_key', 'remote_auth_s3', 'targetWindow', 'sso'];
 var __disqusAdded = false;
 
 function copyProps(context, props) {
@@ -105,7 +105,12 @@ module.exports = _react2.default.createClass({
     * `remote_auth_s3` is needed for Disqus SSO
     *
     */
-    remote_auth_s3: _react2.default.PropTypes.string
+    remote_auth_s3: _react2.default.PropTypes.string,
+    /**
+     * `sso` is an object with properties for a Disqus sso section
+     *
+     */
+    sso: _react2.default.PropTypes.object
 
   },
 
@@ -119,6 +124,7 @@ module.exports = _react2.default.createClass({
       onNewComment: null,
       api_key: null,
       remote_auth_s3: null,
+      sso: null,
       targetWindow: null
     };
   },
